ad83c747e30d18f64b623bef78502f04b9f309c1
arash <ajavanma@rptu.de>
2024-05-28 20:58:05 +0200

    boats to save people

--- leetcode/medium/java/BoatsToSavePeople.java
+++ leetcode/medium/java/BoatsToSavePeople.java
@@ -1,6 +1,7 @@
-package medium.java;
+package leetcode.medium.java;
 
-// You are given an array people where people[i] is the weight of the ith person, and an infinite number of boats where each boat can carry a maximum weight of limit. 
+// You are given an array people, where people[i] is the weight of the ith person, and an infinite number of boats
+// where each boat can carry a maximum weight of limit. 
 // Each boat carries at most two people at the same time, provided the sum of the weight of those people is at most limit.
 
 // Return the minimum number of boats to carry every given person.
@@ -20,14 +21,15 @@ package medium.java;
 // Output: 4
 // Explanation: 4 boats (3), (3), (4), (5)
 
-
 import java.util.Arrays;
-
 
 public class BoatsToSavePeople {
     public int numRescueBoats(int[] people, int limit) {
+        
         Arrays.sort(people);
-        int i = 0, j = people.length - 1;
+
+        int i = 0;
+        int j = people.length - 1;
         int boats = 0;
         
         while (i <= j) {
--- leetcode/medium/python/BoatsToSavePeople.py
+++ leetcode/medium/python/BoatsToSavePeople.py
@@ -1,6 +1,7 @@
 from typing import List
 
-# You are given an array people where people[i] is the weight of the ith person, and an infinite number of boats where each boat can carry a maximum weight of limit. 
+# You are given an array people, where people[i] is the weight of the ith person, and an infinite number of boats 
+# where each boat can carry a maximum weight of limit. 
 # Each boat carries at most two people at the same time, provided the sum of the weight of those people is at most limit.
 
 # Return the minimum number of boats to carry every given person.
@@ -22,8 +23,10 @@ from typing import List
 
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
+        
         people.sort()
-        i, j = 0, len(people) - 1
+        i = 0,
+        j = len(people) - 1
         boats = 0
         
         while i <= j:
